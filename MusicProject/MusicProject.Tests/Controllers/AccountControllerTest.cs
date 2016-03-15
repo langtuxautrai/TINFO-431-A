850c496082885e95c34fb0a0ba2d4e3f2e84a45f
MarkDitianquin <markbsd@uw.edu>
2016-03-14 19:42:01 -0700

    added a simple test for accountcontroller

---
+++ MusicProject/MusicProject.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicProject.Controllers;
+using System.Web.Mvc;
+using MusicProject.Models;
+
+namespace MusicProject.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void Login()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Login("") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ForgotPassword()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ForgotPassword() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ResetPassword()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ResetPassword("") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
